54f658a936c0bcbd5bd06d4651eacaa7f0c29498
abhayks <srivastava.abhay@gmail.com>
2020-08-20 15:32:13 +1000

    Working prototype with Verse

--- frontend/src/components/Geeta/VerseDetails.jsx
+++ frontend/src/components/Geeta/VerseDetails.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import YouTube from 'react-youtube';
 
 export class VerseDetails extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            verse_text: '',
-            verse_text_meaning: '',
-            verse_summary: '',
-            verse_image: '',
+            verse:[],
         };
       }
       componentDidMount() {
@@ -20,20 +18,13 @@ export class VerseDetails extends Component {
         await axios
           .get(`/geeta/verse/${verseid}`)
           .then((res) => {
-              let verse=res.data;
-              console.log("Verse");
-              console.log(verse);
-              console.log(verse.verse_text);
-            this.setState({
-                verse_text: verse.verse_text,
-                verse_text_meaning: verse.verse_text_meaning,
-                verse_summary:verse.verse_summary,
-                verse_image:verse.verse_image,
+              this.setState({
+                verse:res.data,
             });
             console.log("Verse test");
             console.log(res.data);
             console.log("Individual");
-            console.log(this.verse_text);
+            console.log(this.state.verse);
           })
           .catch((err) => {
             console.log("Error fetching Verse Details");
@@ -41,13 +32,23 @@ export class VerseDetails extends Component {
           });
       }
     render() {
+        const verse=this.state.verse;
+        const opts = {
+            height: '290',
+            width: '350',
+            playerVars: {
+              // https://developers.google.com/youtube/player_parameters
+              autoplay: 1,
+            },
+          };
         return (
             <div>
-                This is Verse Details Modified
-                <h2> <i> {this.verse_text} </i></h2>
-                 <h3> {this.verse_text_meaning}</h3>
-               <h4> {this.cverse_summary}</h4>
-                <h5> {this.verse_image} </h5>
+                <h2> <i> {verse.verse_text } </i></h2>
+                <br/>
+                <h3>  {verse.verse_text_meaning } </h3>
+                <h4>  {verse.verse_summary } </h4>
+                <YouTube videoId={verse.verse_image} opts={opts} onReady={this._onReady} />;
+                
             </div>
         )
     }
